f70138e105c6ce0beb058cd911a5cd51882cf472
Charly <carlos.artaza73@gmail.com>
2025-10-30 15:22:18 -0300

    Componente somos

--- src/app/components/somos/somos.component.ts
+++ src/app/components/somos/somos.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-somos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './somos.component.html',
-  styleUrl: './somos.component.css'
+  styleUrls: ['./somos.component.css']
 })
 export class SomosComponent {
-
+  // Puedes agregar variables aquí si necesitas
+  titulo = 'Quiénes Somos';
+  descripcion = 'Somos Importaciones Brisa & Denis - tu tienda de confianza';
 }
