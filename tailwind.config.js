3e04319abc7d53fe4237246c32a814e06d6044a1
Carlos Esteban Artaza <cartaza@buenosaires.gob.ar>
2025-11-05 18:27:27 -0300

    Agregado de tailwind al proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Indicar a Tailwind dónde buscar las clases
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
